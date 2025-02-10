a013a49893725cd568bf91cd3bdb64ec4f380fcf
Frederick <frederick.ennin1@gmail.com>
2025-02-10 13:48:38 +0000

    implemement Dark Mode

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,14 +1,35 @@
-import { HStack, Image, Text } from '@chakra-ui/react'
-import React from 'react'
-import logo from '../assets/webpack.svg'
+"use client";
+import {
+  ClientOnly,
+  HStack,
+  IconButton,
+  Image,
+  Skeleton,
+  Text,
+} from "@chakra-ui/react";
+import React from "react";
+
+import logo from "../assets/webpack.svg";
+import { useColorMode } from "./ui/color-mode";
+import { LuMoon, LuSun } from "react-icons/lu";
 
 function NavBar() {
+  const { toggleColorMode, colorMode } = useColorMode();
   return (
     <HStack>
-        <Image  src={logo} boxSize='60px'/>
-        <Text>Dollarsoft</Text>
+      <Image src={logo} boxSize="60px" />
+      <Text>Dollarsoft</Text>
+      <ClientOnly fallback={<Skeleton boxSize="8" />}>
+        <IconButton
+          onClick={toggleColorMode}
+          variant="outline"
+          size="sm"
+        >
+           {colorMode ==="light" ?<LuSun/> : <LuMoon/>}
+        </IconButton>
+      </ClientOnly>
     </HStack>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
